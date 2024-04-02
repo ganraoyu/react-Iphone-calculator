86b2f2624c11cf2b61f35095f51a071fcc709162
yaoyu <115035161+ganraoyu@users.noreply.github.com>
2024-04-02 18:01:45 -0400

    Update Buttons.jsx

--- project/src/components/Buttons.jsx
+++ project/src/components/Buttons.jsx
@@ -1,18 +1,13 @@
 import React, { useState } from 'react'
 import '../styles/buttons.css'
-function ButtonIcons(){
-  return (
-    <div className='ButtonIcons'>
-    </div>
-  )
-}
 
-const Buttons = () => {
+const Buttons = ( {onButtonClick }) => {
   const [buttons, setButtons] = useState(['C', '±', '%', '÷', 7, 8, 9, '×', 4, 5, 6, '-', 1, 2, 3, '+', 0, '.', '=']);
+
   return (
     <div className='Button'>
       {buttons.map((button, index) => (
-        <button className="button"key={index}>{button}</button>
+        <button className="button"key={index} onClick={() => onButtonClick(button)}>{button}</button>
       ))}
     </div>
   )
