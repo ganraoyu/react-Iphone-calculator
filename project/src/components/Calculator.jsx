ab69c92f0dcf66fc3425eadefb22a9501e576fbd
yaoyu <115035161+ganraoyu@users.noreply.github.com>
2024-04-02 18:01:59 -0400

    Update Calculator.jsx

--- project/src/components/Calculator.jsx
+++ project/src/components/Calculator.jsx
@@ -1,14 +1,64 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Display from './Display'
 import Buttons from './Buttons'
 import '../styles/calculator.css'
 const Calculator = () => {
+  const [displayValue, setDisplayValue] = useState('0');
+  const [operand, setOperand] = useState(null);
+  const [operation, setOperation] = useState(null);
+
+  function handleButtonClick(buttonValue){
+    if (!isNaN(buttonValue)) {
+      if(displayValue != '0') {
+        setDisplayValue(displayValue + buttonValue);
+      } else {
+      setDisplayValue(buttonValue.toString());
+      }
+    } else if (buttonValue === 'C') {
+      setDisplayValue('0');
+    } else if (buttonValue === '=') {
+      if (operation === '/') {
+        setDisplayValue((operand / parseFloat(displayValue)).toString());
+      } else if (operation === '*') {
+        setDisplayValue((operand * parseFloat(displayValue)).toString());
+      } else if (operation === '-') {
+        setDisplayValue((operand - parseFloat(displayValue)).toString());
+      } else if (operation === '+') {
+        setDisplayValue((operand + parseFloat(displayValue)).toString());
+      }
+      setOperand(null);
+      setOperation(null);
+    } else if (buttonValue === '±') {
+      setDisplayValue((displayValue * -1).toString());
+    } else if(buttonValue === '%') {
+      setDisplayValue((displayValue / 100).toString());
+    }  else if (buttonValue === '÷') {
+      setOperand(parseFloat(displayValue));
+      setOperation('/');
+      setDisplayValue('0');
+    } else if (buttonValue === '×') {
+      setOperand(parseFloat(displayValue));
+      setOperation('*');
+      setDisplayValue('0');
+    } else if (buttonValue === '-') {
+      setOperand(parseFloat(displayValue));
+      setOperation('-');
+      setDisplayValue('0');
+    } else if (buttonValue === '+') {
+      setOperand(parseFloat(displayValue));
+      setOperation('+');
+      setDisplayValue('0');
+    } else if (buttonValue === '.') {
+      if (!displayValue.includes('.')) {
+        setDisplayValue(displayValue + '.');
+      }
+    } }
   return (
     <div className='CalculatorDiv'>
-      <Display />
-      <Buttons />
+      <Display value={displayValue}/>
+      <Buttons onButtonClick={handleButtonClick}/>
     </div>
   )
-}
+} 
 
-export default Calculator
+export default Calculator 
